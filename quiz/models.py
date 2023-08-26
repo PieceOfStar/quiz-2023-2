401815c12e6c9f02d72ae20d21dcc2bf78cd2684
MinseoKangQ <kms02171@naver.com>
2023-08-27 03:31:45 +0900

    기능 완성

--- quiz/models.py
+++ quiz/models.py
@@ -5,7 +5,7 @@ CATEGORY = (
     ('normal', 'normal'),
     ('hard', 'hard'),
 )
-
+    
 class Participant(models.Model):
     name = models.CharField(verbose_name = '이름', max_length = 10, null = False)
     student_id = models.CharField(verbose_name = '학번', max_length = 10, null = False)
@@ -19,7 +19,7 @@ class QuizEasy(models.Model):
     choices1 = models.CharField(verbose_name = '선택지1', max_length = 100, default = 'O')
     choices2 = models.CharField(verbose_name = '선택지2', max_length = 100, default = 'X')
     answer = models.CharField(verbose_name = '정답', max_length = 100)
-    sum_score = models.IntegerField(verbose_name = '사용자 누적 점수', default = 0)
+    plus_score = models.IntegerField(verbose_name = '플러스 점수', default = 5)
     
 class QuizNormal(models.Model):
     quiz_num = models.IntegerField(verbose_name = '퀴즈 번호')
@@ -29,7 +29,7 @@ class QuizNormal(models.Model):
     choices2 = models.CharField(verbose_name = '선택지2', max_length = 100)
     choices3 = models.CharField(verbose_name = '선택지3', max_length = 100)
     answer = models.CharField(verbose_name = '정답', max_length = 100)
-    sum_score = models.IntegerField(verbose_name = '사용자 누적 점수', default = 0)
+    plus_score = models.IntegerField(verbose_name = '플러스 점수', default = 10)
     
 class QuizHard(models.Model):
     quiz_num = models.IntegerField(verbose_name = '퀴즈 번호')
@@ -40,4 +40,5 @@ class QuizHard(models.Model):
     answer9_3 = models.CharField(verbose_name = '9번_3 정답', max_length = 10, default = '빅뱅 이론')
     answer10_1 = models.CharField(verbose_name = '10번_1 정답', max_length = 10, default = '405')
     answer10_2 = models.CharField(verbose_name = '10번_2 정답', max_length = 10, default = '405호')
-    sum_score = models.IntegerField(verbose_name = '사용자 누적 점수', default = 0)
+    answer10_3 = models.CharField(verbose_name = '10번_3 정답', max_length = 10, default = '인성관 405호')
+    plus_score = models.IntegerField(verbose_name = '플러스 점수', default = 15)
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import ParticipantForm
-from .models import QuizEasy, QuizNormal, QuizHard
+
+from quiz.forms import ParticipantForm
+from .models import QuizEasy, QuizNormal, QuizHard, Participant
 
 def home_view(request):
     return render(request, 'quiz/home.html')
@@ -14,58 +15,157 @@ def form_view(request):
         
     if request.method == 'POST':
         form = ParticipantForm(request.POST)
+        
         if form.is_valid():
-            form.save()
-            return redirect('quiz:normal', num = 1, user_answer = None)
+            participant = form.save()
+            
+            # 참가자의 학번을 쿠키에 저장
+            response = redirect('quiz:normal', num = 1, user_answer = 'start')
+            response.set_cookie('participant_student_id', participant.student_id)
+            return response
     
     return render(request, 'quiz/form.html') # 등록에 실패한다면 다시 폼 보여주기
 
-def easy_quiz_view(request, num):
+def easy_quiz_view(request, num, user_answer):
     
-    next_num = num + 1
+    next_num = num + 1 # 다음 페이지 전달을 위한 변수
+    participant = get_participant(request = request) # 쿠키를 이용해서 참가자 정보 받아오기
     
-    if (num == 7 or num <= 8): # 7 혹은 8이면 quiz-easy.html
+    # 7 혹은 8이면 quiz-easy.html
+    if (num == 7 or num == 8): 
         
-        item = QuizEasy.objects.get(quiz_num = num)
+        item = QuizEasy.objects.get(quiz_num = num) # 퀴즈 가져오기
+        
+        # num이 8 이면
+        if(num == 8):
+            previous_item = QuizEasy.objects.get(quiz_num = num-1) # 전 퀴즈 가져오기
+            
+            # 사용자의 정답과 퀴즈 정답이 일치한다면 
+            if(user_answer == str(previous_item.answer)):
+                participant.score += item.plus_score # 참가자의 점수 올리기
+                participant.save() # 저장
+                print('7번 정답, 점수 올림')
+                
         context = { 
             'item' : item,
             'next_num' : next_num, 
         }
+        print(participant.score)
         return render(request, 'quiz/quiz-easy.html', context)
     
     else:
+        previous_item = QuizEasy.objects.get(quiz_num = num-1) # 전 퀴즈 가져오기
+            
+        # 사용자의 정답과 퀴즈 정답이 일치한다면 
+        if(user_answer == str(previous_item.answer)):
+            participant.score += 5 # 참가자의 점수 올리기
+            participant.save() # 저장
+            print('8번 정답, 점수 올림')
+        
+        print(participant.score)
         return redirect('quiz:hard', num = 9)
 
-def normal_quiz_view(request, num, user_answer = None):
+def normal_quiz_view(request, num, user_answer):
     
-    print(user_answer)
-    print(request)
-    next_num = num + 1
+    next_num = num + 1 # 다음 페이지 전달을 위한 변수
+    participant = get_participant(request = request) # 쿠키를 이용해서 참가자 정보 받아오기
     
-    if (num >= 1 and num <= 6): # 1~6이면 quiz-normal.html
+    # 1~6이면 quiz-normal.html
+    if (num >= 1 and num <= 6): 
         
-        item = QuizNormal.objects.get(quiz_num = num)
-        
+        item = QuizNormal.objects.get(quiz_num = num) # 퀴즈 가져오기
+    
+        # num이 2 이상이면
         if(num >= 2):
-            previous_item = QuizNormal.objects.get(quiz_num = num-1)
-            print(previous_item.answer)
+            previous_item = QuizNormal.objects.get(quiz_num = num-1) # 전 퀴즈 가져오기
+            
+            # 사용자의 정답과 퀴즈 정답이 일치한다면 
             if(user_answer == str(previous_item.answer)):
-                print("정답")
-                item.sum_score += 10
+                participant.score += item.plus_score # 참가자의 점수 올리기
+                participant.save() # 저장
         
+        # 전달
         context = { 
             'item' : item,
             'next_num' : next_num,
         }
+        
+        print(participant.score)
+        
         return render(request, 'quiz/quiz-normal.html', context)
     
     else:
-        return redirect('quiz:easy', num = 7)
+        previous_item = QuizNormal.objects.get(quiz_num = num-1) # 전 퀴즈 가져오기
+            
+        # 사용자의 정답과 퀴즈 정답이 일치한다면 
+        if(user_answer == str(previous_item.answer)):
+            participant.score += 10 # 참가자의 점수 올리기
+            participant.save() # 저장
+        
+        print(participant.score)
+        return redirect('quiz:easy', num = 7, user_answer = 'start')
     
-
 def hard_quiz_view(request, num):
-    # form 보여주기
-    pass
+    
+    item = QuizHard.objects.get(quiz_num = num) # 퀴즈 가져오기
+    participant = get_participant(request = request) # 쿠키를 이용해서 참가자 정보 받아오기
+    
+    if request.method == 'POST':
+        
+        user_answer = request.POST.get('user_answer', None)
+        if user_answer is not None:
+            
+            if num == 9:
+                answer_dict = { 'answer9_1': item.answer9_1, 'answer9_2': item.answer9_2, 'answer9_3': item.answer9_3, }
+                
+                # for문을 돌며 user_answer와 비교
+                for answer_key, correct_answer in answer_dict.items():
+                    if user_answer == correct_answer: # 정답이면
+                        participant.score += item.plus_score # 참가자의 점수 올리기
+                        participant.save() # 저장
+                
+                # form 보여주기
+                context = {
+                    'item' : item,
+                    'num' : 10,
+                }
+                
+                print(participant.score)
+                return render(request, 'quiz/quiz-hard.html', context)
+            
+            else:
+                answer_dict = { 'answer10_1': item.answer10_1, 'answer10_2': item.answer10_2, 'answer10_3': item.answer10_3, }
+                # for문을 돌며 user_answer와 비교
+                for answer_key, correct_answer in answer_dict.items():
+                    if user_answer == correct_answer: # 정답이면
+                        participant.score += item.plus_score # 참가자의 점수 올리기
+                        participant.save() # 저장
+
+                # form 보여주기
+                context = {
+                    'item' : item,
+                    'num' : num,
+                }
+                
+                print(participant.score)
+                return render(request, 'quiz/result.html', context)
+
+    context = {
+                    'item' : item,
+                    'num' : num,
+                }
+    return render(request, 'quiz/quiz-hard.html', context)
 
 def result_view(request):
     return render(request, 'quiz/result.html')
+
+# 참가자 정보 가져오기
+def get_participant(request):
+    
+    participant_student_id = request.COOKIES.get('participant_student_id')
+
+    try:
+        return Participant.objects.get(student_id=participant_student_id)
+        
+    except Participant.DoesNotExist:
+        print("참가자 정보를 찾을 수 없습니다.")
