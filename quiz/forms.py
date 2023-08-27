4e099415e739e2f06b3cea54b9d658d6e6a2974e
MinseoKangQ <kms02171@naver.com>
2023-08-27 17:14:32 +0900

    form.html css 적용 완료

--- quiz/forms.py
+++ quiz/forms.py
@@ -6,7 +6,19 @@ class ParticipantForm(forms.ModelForm):
         model = Participant
         fields = ['name', 'student_id', 'phone_num']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'input__name'}),
-            'student_id': forms.TextInput(attrs={'class': 'input__student_id'}),
-            'phone_num': forms.TextInput(attrs={'class': 'input__phone_num'}),
+            'name': forms.TextInput(attrs={
+                'class' : 'input__name',
+                'id' : 'id_name',
+                'placeholder' : '이름을 작성해주세요.'
+                }),
+            'student_id': forms.TextInput(attrs={
+                'class': 'input__student_id',
+                'id': 'id_student_id',
+                'placeholder' : '학번을 작성해 주세요. (ex.2171234)'
+                }),
+            'phone_num': forms.TextInput(attrs={
+                'class': 'input__phone_num',
+                'id': 'id_phone_num',
+                'placeholder' : '전화번호를 작성해 주세요. (ex.01000000000)'
+                }),
         }
