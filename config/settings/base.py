a68b50e24f7951efbd837bb835b7b915bc4c8434
MinseoKangQ <kms02171@naver.com>
2023-08-27 22:08:13 +0900

    배포 준비

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,34 +10,49 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import json
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r2w^&i)hc0t9*!hl@47yc1%vrg53j=m7x9q2u9xveno4ivg6c('
+secret_file = BASE_DIR / 'secrets.json'
+with open(secret_file) as file:
+    secrets = json.loads(file.read())
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+def get_secret(setting, secrets_dict=secrets):
+    try:
+        return secrets_dict[setting]
+    except KeyError:
+        error_msg = f'Set the {setting} environment variable'
+        raise ImproperlyConfigured(error_msg)
+    
+SECRET_KEY = get_secret('SECRET_KEY')
+print(f'SECRET_KEY = {get_secret("SECRET_KEY")}')
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [ 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+PROJECT_APPS = [
     'quiz',
+]
+
+THIRD_PARTY_APPS = [
+    
 ]
 
 MIDDLEWARE = [
@@ -70,18 +85,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -99,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -122,9 +124,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 개발자가 추가하는 부분 (ex. css파일, js파일 등)
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / 'static' 
-]
 
 # 사용자가 추가하는 부분 (ex. 프로필 사진, 게시글 사진 등)
 MEDIA_URL = '/media/'
