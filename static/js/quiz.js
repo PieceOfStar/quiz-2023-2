41582ab1287f972dbb2a9772f13fc35c1b84ca83
MinseoKangQ <kms02171@naver.com>
2023-08-28 14:09:50 +0900

    버튼 수정중

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const choices = document.querySelectorAll('.quiz__c');
+    const links = document.querySelectorAll('a .quiz__c');
 
-    // 각 요소에 클릭 이벤트 리스너를 추가합니다.
-    choices.forEach(choice => {
-        choice.addEventListener('click', function() {
-            // 먼저 모든 선택을 제거합니다.
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // 기본 링크 동작 중단
+            const div = link.closest('.quiz__c');
+            const choices = document.querySelectorAll('.quiz__c');
+
             choices.forEach(c => c.classList.remove('selected'));
-            
-            // 클릭된 요소에 'selected' 클래스를 추가합니다.
-            this.classList.add('selected');
+            div.classList.add('selected');
         });
     });
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const choices = document.querySelectorAll('.quiz__c');
+
+//     // 각 요소에 클릭 이벤트 리스너를 추가합니다.
+//     choices.forEach(choice => {
+//         choice.addEventListener('click', function() {
+//             // 먼저 모든 선택을 제거합니다.
+//             choices.forEach(c => c.classList.remove('selected'));
+            
+//             // 클릭된 요소에 'selected' 클래스를 추가합니다.
+//             this.classList.add('selected');
+//         });
+//     });
+// });
