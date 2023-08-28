863976ca111e308febe15a299efed03504abbdcc
MinseoKangQ <kms02171@naver.com>
2023-08-28 16:53:19 +0900

    버튼 수정 완료

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -3,12 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // 각 요소에 클릭 이벤트 리스너를 추가합니다.
     choices.forEach(choice => {
-        choice.addEventListener('click', function() {
+        choice.addEventListener('click', function(event) {
+            // 클릭 이벤트의 기본 동작을 중단합니다.
+            event.preventDefault();
+
             // 먼저 모든 선택을 제거합니다.
             choices.forEach(c => c.classList.remove('selected'));
             
             // 클릭된 요소에 'selected' 클래스를 추가합니다.
             this.classList.add('selected');
+            
+            // 1초 딜레이 후 페이지 전환을 수행합니다.
+            setTimeout(() => {
+                window.location.href = this.parentElement.getAttribute('href'); // 다음 페이지로 이동
+            }, 350);
         });
     });
 });
